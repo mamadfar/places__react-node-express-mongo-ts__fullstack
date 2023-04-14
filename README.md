0b8cd9c19634861285b04426c2c2dd5d1e831df5
mohammad farhadi <m.sabah.farhadi@gmail.com>
2023-04-14 18:59:34 +0200

    add places and users routes to the backend

---
+++ README.md
@@ -0,0 +1,5 @@
+# ?
+Install dependencies in the both side (Front and Back)
+```shell
+npm i
+```
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,21 @@
 import express from "express";
 
+import placesRoutes from "./routes/places.route";
+import usersRoutes from "./routes/users.route";
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+//? Configuration
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//? Routes
+app.use("/places", placesRoutes);
+app.use("/users", usersRoutes);
 
 //? Health Route
 app.get("/ping", (req, res) => res.status(200).json("Pong"));
 
+//? Server
 app.listen(PORT, () => console.log("Server is running..."));
